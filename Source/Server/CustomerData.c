bf2a21a87f2966729559d9c980c45b2d81b11dd0
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-11-15 06:56:25 -0600

    Added CustomerData struct.
    This struct is designed to help hold customer data that will be relevant within the program.  The customer data will be required for simulation of purchasing items and logging into the services.

---
+++ Source/Server/CustomerData.c
@@ -0,0 +1,18 @@
+typedef struct
+{
+    char* firstName;            // The user's First Name
+    char* lastName;             // The user's Last Name
+    char* userID;               // The user's Sign-In and User Name for the service.
+    char* userKey;              // The user's Password in a non-secured fashion.
+        // SECURITY NOTE: Because the password is in _RAW_TEXT_ form, security is NOT
+        //  being practiced here.  This is merely a simulation program, not a REAL WORLD
+        //  Application!  Please take that into consideration.  [NG]
+    char* email;                // The user's Email Address.
+    char* phoneNumber;          // The user's Phone Number.
+    char* addressCity;          // The user's Address: City/Village
+    char* addressState;         // The user's Address: State
+    char* addressCountry;       // The user's Address: Country
+    char* addressStreet;        // The user's Address: Street
+    char* addressPostalCode;    // The user's Address: Postal Code
+    int admin;                  // Is this user part of the Administration team?
+} CustomerData;
