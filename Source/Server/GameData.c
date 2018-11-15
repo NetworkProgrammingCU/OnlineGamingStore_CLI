f656fbcce41684a6a3942a4914a69ee684f1c3d0
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-11-15 06:58:29 -0600

    Added Game Data struct.
    This holds the game product data within the store model.  The information provided within the store, will allow the customer to browser through the products and view the provided information.

---
+++ Source/Server/GameData.c
@@ -0,0 +1,9 @@
+typedef struct
+{
+    char* title;            // Title of the game
+    char* description;      // Description of the game
+    char* publisher;        // Publisher of the game
+    char* developers;       // Developers (or company) that created the game
+    char* genre;            // Genre of the game
+    char* notes;            // Specialized notes regarding the game
+} GameData;
