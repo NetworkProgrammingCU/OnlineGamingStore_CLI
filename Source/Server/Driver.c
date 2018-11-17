7be4d6c4cb196059d178da06e5fb431b17b4e39a
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-11-17 17:02:54 -0600

    Added header to the Server main source file
    Provided the header comment block to the main source file to the server application.

---
+++ Source/Server/Driver.c
@@ -0,0 +1,26 @@
+// ============================================
+// Programmers:     Nicholas Gautier
+//                  Justin Griffith
+//                  Marilyn Li
+// Class:           CS3013; Network Programming
+// Assignment #:    3
+// Due Date:        12.December.2018
+// Instructor:      Dr. Zhao
+// Description:     TO-BE-DETERMINED
+// Credits:
+//  Base Source Code provided by W. Richard Stevens in
+//      Unix Network Programming, The Sockets Networking API
+//      Volume 1, 3rd edition.  All credit goes to him for the framework
+// Return Codes:
+//  0 = Successful Operation
+// ============================================
+
+
+
+// Included Libraries
+// ===============================
+#include "myunp.h"              // Our custom UNP Header file
+#include "CommonFunctions.c"    // This contains our common functions
+                                //  will be available through out the
+                                //  entire program.
+// ===============================
