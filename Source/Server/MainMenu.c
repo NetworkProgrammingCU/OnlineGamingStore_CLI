d403c0bcab48fc8a5b2c7cd12a26eb0335231ff7
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-12-07 12:10:15 -0600

    Revert "Changed "x" to "exit""
    This reverts commit 2e5bc52d35c8ad6dcf63466b35454c8e2dc8ee02.

--- Source/Server/MainMenu.c
+++ Source/Server/MainMenu.c
@@ -15,7 +15,6 @@
 #include "GlobalDefs.h"         // Program Macro-Definitions
 #include "CommonFunctions.h"    // Shared functions; to help minimize development cost between sub-projects.
 #include "MainMenu.h"           // Main Menu
-#include "myunp.h"				// For read()/write() and MAXLINE
 // ===============================
 
 
@@ -34,12 +33,12 @@
 //  2 = Exit
 //  3 = Bad request
 // -----------------------------------
-int MainMenu(int sockfd)
+int MainMenu()
 {
-    DrawInstructionsMainMenu(sockfd);     // Draw Instructions
+    DrawInstructionsMainMenu();     // Draw Instructions
                                     //  regarding the main menu.
-    DrawMenuMain(sockfd);                 // Provide the Main Menu
-    return MainMenuUserRequest(sockfd);   // Fetch user input
+    DrawMenuMain();                 // Provide the Main Menu
+    return MainMenuUserRequest();   // Fetch user input
 } // MainMenu()
 
 
@@ -48,96 +47,6 @@ int MainMenu(int sockfd)
 // Main Menu User Request
 // -----------------------------------
 // Documentation:
-//  This will allow the user to provide input in regards to
-//  the main menu, but the input will also be modified to work
-//  within this program.
-// -----------------------------------
-// Output:
-//  0 = Store Page
-//  1 = Update User Information
-//  2 = Exit
-//  3 = Bad request
-// -----------------------------------
-int MainMenuUserRequest(int sockfd)
-{
-    // Declarations and Initializations
-    // ----------------------------------
-    char userInput[_MAX_CHAR_INPUT_];    // This will hold the user input.
-    // ----------------------------------
-    
-    // Display the prompt
-    DisplayPrompt(sockfd);
-    
-    // Get the user input
-    read(sockfd, userInput, _MAX_CHAR_INPUT_);
-    
-    // Lower case the user's input
-    LowerCaseUserInput(userInput);
-    
-    // Try to determine the user's request
-    if (userInput[0] == '1')
-        // Store Page
-        return 0;
-    else if (userInput[0] == '2')
-        // Update User Information
-        return 1;
-    else if (userInput[0] == 'x')
-        // Exit from the Store
-        return 2;
-    else if (!CheckForUserQuit(userInput, _MAX_CHAR_INPUT_))
-        // Exit from the store (Exit or Quit keywords)
-        return 2;
-    else
-        // Unknown Request
-        return 3;    
-} // MainMenuUserRequest()
-
-
-
-
-// Draw Main Menu
-// -----------------------------------
-// Documentation:
-//  This will provide the main menu to the user.
-// -----------------------------------
-void DrawMenuMain(int sockfd)
-{
-	char sendbuffer[MAXLINE];
-	ClearBuffer(sendbuffer, MAXLINE);
-	
-    strcpy(sendbuffer, "Main Menu\n------------------------------------------------\n\n");
-    // View Store Catalog
-    strcat(sendbuffer, "[1] - View Game Store\n");
-    strcat(sendbuffer, "       View what games are available within our store!\n");
-    // Update Personal Information
-    strcat(sendbuffer, "[2] - Update Personal Information\n");
-    strcat(sendbuffer, "       View and change your personal account settings, such as email, address, etc.\n");
-    // Leave terminate session
-    strcat(sendbuffer, "[Exit] - Leave Store\n");
-    strcat(sendbuffer, "       Exit from the store\n");
-	
-	write(sockfd, sendbuffer, MAXLINE);
-} // DrawMenuMain()
-
-
-
-
-// Draw Instructions
-// -----------------------------------
-// Documentation
-//  This function will provide the instructions to the client,
-//  in regards into how to use the main menu screen.
-// -----------------------------------
-void DrawInstructionsMainMenu(int sockfd)
-{
-	char sendbuffer[MAXLINE];
-	ClearBuffer(sendbuffer, MAXLINE);
-	
-    sprintf(sendbuffer, "Select the following options from the screen:\n");
-	write(sockfd, sendbuffer, MAXLINE);
-	
-} // DrawInstructions()
-
 //  This will allow the user to provide input in regards to
 //  the main menu, but the input will also be modified to work
 //  within this program.
